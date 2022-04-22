77885b0024a70d1e4a369c5e7f34d228f94e2c5c
ahmetdoruk <ahmet_eagle1903@hotmail.com>
2022-04-22 21:34:33 +0300

    AC is completed

--- src/LoginTest_Ahmet.java
+++ src/LoginTest_Ahmet.java
@@ -3,5 +3,7 @@ public class LoginTest_Ahmet {
     public static void main(String[] args) {
 
         System.out.println("Initial change and first class ");
+
+        System.out.println("AC1 testing with valid credentials is passed");
     }
 }
